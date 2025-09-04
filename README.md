b30918b74376ab94359786f92313bf6f00768b09
Motok1 <m.takenaka@ntt.com>
2025-09-04 02:11:26 +0000

    refactor

--- README.md
+++ README.md
@@ -1,37 +1,27 @@
 # eve2clab
 
-A rudimentary script to convert an EVE-NG .unl file to a containerlab topology. 
-
-It can also be used to 'extract' startup confgs from an EVE-NG `.unl` file.
+EVE-NG で作成したトポロジ情報(.unl)を clab の config(.clab.yaml) に変更するためのツール
 
 ## Usage
 
-1. Place the EVE-NG `.unl` file in the same directory as `parse.py`.
-1. In `parse.py` edit:
-    - `REGISTRY` to your container registry (usually this should be `vrnetlab`).
-    - `file_path` to the name of your `.unl` file (including file extension).
+1. EVE-NG から export した .unl を用意する。変換時にファイルの場所を指定するため、置く場所はどこでも良い。
+2. `python3 converter.py -f <unl ファイル>` を実行する。実行後、`export-**` のディレクトリが生成され、ディレクトリ内に .clab.yaml も生成される。
 
-1. Run `parse.py`. A new export directory with the lab name should be created. In that directory you will find:
-
-- Converted containerlab topology
-- Any startup configurations under the 'config' directory.
+※ `--mode` option で変換モードが指定できるようになっているが、現時点では unl → clab の変換のみで逆は NotIMprementedError となる。
 
 ## Caveats
 
-**Mapping**
+**unl の instance type と clab の kind のマッピングについて**
 
-You may have to edit the `MAPPING` dictionary. `MAPPING` is used to convert the EVE-NG template name to the correct containerlab kind. 
-
-It is simple to add a mapping and the format is as follows:
+`MAPPING` の辞書において、 instance type の変換表を保持している。
+新規に変換を増やす場合や、mapping を変更したい場合は辞書の中を編集して対応を更新すること。
 
 ```py
 MAPPING = {
-    'EVE-NG Name': 'Containerlab Kind'
+    'EVE-NG instance prefix': 'Containerlab Kind'
 }
  ```
 
 **Links**
 
-1. Only point-to-point links are supported, currently multipoint or networks connected via 'dumb' switches aren't supported.
-
-1. The topology `yaml` file will not contain the links in the abbreviated format you are used to. Indentation will also be incorrect. These two issues are mainly visual and the topology will work with containerlab as normal.
+1. P2P のみのサポートで、L2 NW の変換はできません。
---
+++ unl2clab.py
@@ -0,0 +1,126 @@
+import xml.etree.ElementTree as ET
+import re
+import yaml
+import base64
+import os
+
+# {"EVE-NG のテンプレート名": "Containerlab の kind 名"}
+MAPPING = {
+    "xrv": "cisco_xrv",
+    "xrv9k": "cisco_xrv9k",
+    "csr1000v": "cisco_csr1000v",
+    "csr1000vng": "cisco_csr1000v",
+    "cat8000v": "cisco_c8000v",
+    "vios": "linux",
+    "vjunosevoefi": "juniper_vjunosevolved",
+    "vjunosevo": "juniper_vjunosevolved",
+    "vjunosevo23": "juniper_vjunosevolved",
+}
+
+REGISTRY = "vrnetlab"
+
+def __parse_unl(file_path):
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    return root
+
+
+def convert_unl_to_clab(file_path):
+    root = __parse_unl(file_path)
+    # lab の名前
+    name = "".join([ c if c.isalnum() else "-" for c in root.get("name") ]).lower()
+    # clab config が置かれるディレクトリの名前
+    # .gitignore で除外している
+    export_dir = f"./export-{name}"
+
+    clab_topo = {
+        'name': name,
+        'topology': {},
+    }
+
+    raw_nodes = {}
+    links = {}
+    # {"EVE の image名": {"image": "<registry>/<node_kind>","version": "container の version tag"} }
+    # topo から node 情報と link 情報を取得
+    for node in root.find("topology").find("nodes").findall("node"):
+        
+        node_id = node.get("id")
+        node_name = node.get("name")
+        node_tmpl = node.get("template")
+        
+        if node_tmpl in MAPPING:
+            node_kind = MAPPING[node_tmpl]
+        else:
+            print(f"{node_name}:\tKind \x1B[0;33m'{node_tmpl}'\x1b[0m is \x1B[0;31mnot found\x1b[0m in mapper")
+        
+        node_image_raw = node.get("image")
+        # image 名からバージョン情報をとってきている
+        node_image_version = re.search("(?<=-).*$", node_image_raw).group()
+        node_image = f"{REGISTRY}/{node_kind}:{node_image_version}"
+
+
+        raw_nodes[node_id] = {'name': node_name, 'kind': node_kind, 'image': node_image}
+
+        for interface in node.findall('interface'):
+            intf_id = interface.get("id")
+            intf_name = interface.get("name")
+            intf_net = interface.get("network_id")
+            intf_obj = {'node': node_name, 'interface': intf_name, 'intf_idx': intf_id}
+
+            if intf_net not in links:
+                links[intf_net] = {}
+                
+            links[intf_net][node_name] = intf_obj
+    
+    # startup config を取得
+    if root.find("objects") is not None:
+        for config in root.find("objects").find("configs").findall("config"):
+            cfg_id = config.get("id")
+            raw_nodes[cfg_id]['config'] = config.text
+
+    link_list = []
+    nodes = {}
+
+    try:
+        os.makedirs(export_dir)
+        os.makedirs(f"{export_dir}/configs")
+    except:
+        pass
+
+    for x in raw_nodes:
+        node_name = raw_nodes[x]['name']
+        node_kind = raw_nodes[x]['kind']
+        node_image = raw_nodes[x]['image']
+
+        if 'config' in raw_nodes[x]:
+            cfg_rel_path = f"./configs/{node_name}.cfg"
+            cfg_exp_path = f"{export_dir}/configs/{node_name}.cfg"
+            
+            print(f"{node_name}:\tConfiguration \x1B[0;32mfound...\x1B[0m Attempting to write to {cfg_exp_path}", end='')
+            
+            try:
+                with open(cfg_exp_path, "w") as cfg:
+                    cfg.write(base64.b64decode(raw_nodes[x]['config']).decode())
+                print(f" \t\x1B[0;32mSUCCESS\x1B[0m")
+            except:
+                print(f" \t\x1B[0;31mFAIL\x1B[0m")
+            nodes[node_name] = {'kind': node_kind,'image': node_image, 'startup-config': cfg_rel_path}
+        else:
+            nodes[node_name] = {'kind': node_kind, 'image': node_image}
+
+    for x in links:
+        keys = list(links[x].keys())
+        intf0 = f"{keys[0]}:{links[x][keys[0]]['interface']}"
+        intf1 = f"{keys[1]}:{links[x][keys[1]]['interface']}"
+        
+        link_list.append({'endpoints': [intf0, intf1]})
+
+    clab_topo['topology']['nodes'] = nodes
+    clab_topo['topology']['links'] = link_list
+
+    print("Exporting to 'topo.clab.yml'")
+
+    with open(f'{export_dir}/topo.clab.yml', 'w') as yaml_file:
+        yaml.dump(clab_topo, yaml_file, default_flow_style=False, sort_keys=False)
+
+    print(f"Done {name}")
